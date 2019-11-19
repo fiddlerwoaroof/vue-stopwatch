8090b9ff91c4de03aa4ef91549a7a76de0bd4f48
Ed Langley <el-github@elangley.org>
2019-11-18 16:38:50 -0800

    chore: bump stuff

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-  new VueLoaderPlugin(),
+    new VueLoaderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -81,6 +81,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     }),
-    new VueLoaderPlugin()
   ])
 }
